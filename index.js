6aced283d31e2dc1b586248e11cae1ed68308c84
Akdrex <roman.gaignault@edu.devinci.fr>
2022-11-17 20:38:29 +0100

    TD3

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const locationController = require('./locations/locations.controller')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.use(locationController)
+app.use(locationController);
 
-app.listen(port, () => {
-	console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
-})
+
+async function main(){
+	await mongoose.connect(process.env.MONGO_URI);
+	app.listen(port, () => {
+		console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
+	})
+}
+main();
+
+
+
+
